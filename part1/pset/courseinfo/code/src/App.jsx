dacff47c2cdc19f2291a322e8ccba19ba79bdec5
ugochukwu-850 <ugochukwuchizaramoku@gmail.com>
2023-11-03 16:33:36 +0000

    Completed Pset 1.5 CourseInfo

---
+++ part1/pset/courseinfo/code/src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+const Part = ({ part, exercises }) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      <Part part={parts[0].name} exercises={parts[0].exercises} />
+      <Part part={parts[1].name} exercises={parts[1].exercises} />
+      <Part part={parts[1].name} exercises={parts[1].exercises} />
+    </>
+  )
+
+}
+
+
+const Total = ({ parts }) => {
+  let total = 0;
+  parts.forEach(e => {
+    total += e.exercises
+  });
+
+  return (
+    <p>Number of exercises {total}</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App
